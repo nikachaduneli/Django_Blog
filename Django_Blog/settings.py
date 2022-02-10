5cf704bc5184650fe60cb7d97eb39d3f6eec540c
nikachaduneli <nikoloz.chaduneli.1@btu.edu.ge>
2022-02-11 00:33:44 +0400

    inital commit

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Django_Project.urls'
+ROOT_URLCONF = 'Django_Blog.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Django_Project.wsgi.application'
+WSGI_APPLICATION = 'Django_Blog.wsgi.application'
 
 
 # Database
