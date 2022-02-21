6ffe33ca21a5aa189b8310872635b288b2a065cc
nikachaduneli <nikoloz.chaduneli.1@btu.edu.ge>
2022-02-21 22:37:27 +0400

    Updated Settings.py

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chadunelinika15@gmail.com'
-EMAIL_HOST_PASSWORD = 'wnrdpehaaniflant'
+EMAIL_HOST_USER = os.environ.get("EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("PASSWORD")
